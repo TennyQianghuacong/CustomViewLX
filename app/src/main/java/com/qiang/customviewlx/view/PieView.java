d2fe9b7350f90eee7932e6a79d1ef61d4d018d7d
TennyQianghuacong <dreamer.qianghc@gmail.com>
2020-04-04 17:18:12 +0800

    upgrade pieView

--- app/src/main/java/com/qiang/customviewlx/view/PieView.java
+++ app/src/main/java/com/qiang/customviewlx/view/PieView.java
@@ -47,7 +47,7 @@ public class PieView extends View {
         RectF rectF1 = new RectF(rectFWith / 2, getWidth() / 8, rectFWith / 2 + rectFWith,  getWidth() / 8 + rectFWith);
 
         paint.setColor(ContextCompat.getColor(getContext(), R.color.yellow_500));
-        canvas.drawArc(rectF1, - 30 + 0.5f, 30 - 0.5f, true, paint);
+        canvas.drawArc(rectF1, - 60 + 0.5f, 60 - 0.5f, true, paint);
 
         paint.setColor(ContextCompat.getColor(getContext(), R.color.blue_700));
         canvas.drawArc(rectF1, 0 + 0.5f, 120 - 0.5f, true, paint);
@@ -62,6 +62,45 @@ public class PieView extends View {
         RectF rectF2 = new RectF(rectFWith / 2 - offsetX, getWidth() / 8 - offsetY, rectFWith / 2 + rectFWith - offsetX,  getWidth() / 8 + rectFWith - offsetY);
 
         paint.setColor(ContextCompat.getColor(getContext(), R.color.red_700));
-        canvas.drawArc(rectF2, 180 + 0.5f, 150 - 0.5f, true, paint);
+        canvas.drawArc(rectF2, 180 + 0.5f, 120 - 0.5f, true, paint);
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(27);
+        canvas.drawText("饼图", rectFWith / 2 + rectFWith /2 * 0.9f, getWidth() * 1.4f/ 8 + rectFWith, paint);
+
+        float horizontalWith = ViewUtil.dip2px(getContext(), 30);
+
+        paint.setStrokeWidth(ViewUtil.dip2px(getContext(), 1));
+        float yellowPointStartX = (float) (rectFWith + (rectFWith / 2) * Math.cos(Math.PI * 1 / 6) );
+        float yellowPointStartY = (float) (getWidth() / 8 + rectFWith / 2 - rectFWith * Math.sin(Math.PI * 1 / 6) / 2);
+        float yellowPointEndX = (float) (rectFWith + (rectFWith / 2) * 1.2 * Math.cos(Math.PI * 1 / 6) );
+        float yellowPointEndY = (float) (getWidth() / 8 + rectFWith / 2 - rectFWith * 1.2 * Math.sin(Math.PI * 1 / 6) / 2);
+
+        float[] yellowPoint = {yellowPointStartX, yellowPointStartY, yellowPointEndX, yellowPointEndY, yellowPointEndX, yellowPointEndY, yellowPointEndX + horizontalWith, yellowPointEndY};
+        canvas.drawLines(yellowPoint, paint);
+        canvas.drawText("yellow", yellowPointEndX + 10, yellowPointEndY - 10, paint);
+
+        float bluePointStartX = (float) (rectFWith + (rectFWith / 2) * Math.cos(Math.PI * 1 / 3) );
+        float bluePointStartY = (float) (getWidth() / 8 + rectFWith / 2 + rectFWith * Math.sin(Math.PI * 1 / 3) / 2);
+        float bluePointEndX = (float) (rectFWith + (rectFWith / 2) * 1.2 * Math.cos(Math.PI * 1 / 3) );
+        float bluePointEndY = (float) (getWidth() / 8 + rectFWith / 2 + rectFWith * 1.2 * Math.sin(Math.PI * 1 / 3) / 2);
+        float[] bluePoint = {bluePointStartX, bluePointStartY, bluePointEndX, bluePointEndY, bluePointEndX, bluePointEndY, bluePointEndX + horizontalWith, bluePointEndY};
+        canvas.drawLines(bluePoint, paint);
+        canvas.drawText("blue", bluePointEndX + 12, bluePointEndY - 10, paint);
+
+        float greenPointStartX = (float) (rectFWith - (rectFWith / 2) * Math.cos(Math.PI * 1 / 6) );
+        float greenPointStartY = (float) (getWidth() / 8 + rectFWith / 2 + rectFWith * Math.sin(Math.PI * 1 / 6) / 2);
+        float greenPointEndX = (float) (rectFWith - (rectFWith / 2) * 1.2 * Math.cos(Math.PI * 1 / 6) );
+        float greenPointEndY = (float) (getWidth() / 8 + rectFWith / 2 + rectFWith * 1.2 * Math.sin(Math.PI * 1 / 6) / 2);
+        float[] greenPoint = {greenPointStartX, greenPointStartY, greenPointEndX, greenPointEndY, greenPointEndX, greenPointEndY, greenPointEndX - horizontalWith, greenPointEndY};
+        canvas.drawLines(greenPoint, paint);
+        canvas.drawText("green", greenPointEndX - horizontalWith + 12, greenPointEndY - 10, paint);
+
+        float redPointStartX = (float) (rectFWith - (rectFWith / 2) * Math.cos(Math.PI * 1 / 3) - offsetX);
+        float redPointStartY = (float) (getWidth() / 8 + rectFWith / 2 - rectFWith * Math.sin(Math.PI * 1 / 3) / 2 - offsetY);
+        float redPointEndX = (float) (rectFWith - (rectFWith / 2) * 1.2 * Math.cos(Math.PI * 1 / 3) - offsetX);
+        float redPointEndY = (float) (getWidth() / 8 + rectFWith / 2 - rectFWith * 1.2 * Math.sin(Math.PI * 1 / 3) / 2 - offsetY);
+        float[] redPoint = {redPointStartX, redPointStartY, redPointEndX, redPointEndY, redPointEndX, redPointEndY, redPointEndX - horizontalWith, redPointEndY};
+        canvas.drawLines(redPoint, paint);
+        canvas.drawText("red", redPointEndX - horizontalWith + 12, redPointEndY - 10, paint);
     }
 }
