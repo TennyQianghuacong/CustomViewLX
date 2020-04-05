b072b4e4869653de5961496df6b7a8b91dda84f9
TennyQianghuacong <dreamer.qianghc@gmail.com>
2020-04-06 01:11:34 +0800

    upgrade text LX

--- app/src/main/java/com/qiang/customviewlx/view/TextLXView.java
+++ app/src/main/java/com/qiang/customviewlx/view/TextLXView.java
@@ -2,8 +2,14 @@ package com.qiang.customviewlx.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.graphics.Xfermode;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -59,7 +65,17 @@ public class TextLXView extends View {
         paint.setFakeBoldText(false);
         offsetY += paint.getFontSpacing();
         paint.setTypeface(Typeface.DEFAULT);
-        float advance = paint.getRunAdvance(text, 0, text.length(), 0, text.length(), false, text.length());
+
+        Rect textBounds = new Rect();
+        paint.getTextBounds(text, 0, text.length() - 3, textBounds);
+        textBounds.left += offsetX;
+        textBounds.right += offsetX;
+        textBounds.top += offsetY;
+        textBounds.bottom += offsetY;
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(textBounds, paint);
+        paint.setStyle(Paint.Style.FILL);
+        float advance = paint.getRunAdvance(text, 0, text.length(), 0, text.length(), false, 2);
         canvas.drawText(text, offsetX, offsetY, paint);
         canvas.drawLine(offsetX + advance, offsetY - 48, offsetX + advance, offsetY, paint);
 
@@ -68,6 +84,105 @@ public class TextLXView extends View {
         offsetY += paint.getFontSpacing();
         float textWith = paint.measureText(text);
         canvas.drawLine(offsetX, offsetY, offsetX + textWith, offsetY, paint);
+        canvas.drawText(text, offsetX, offsetY, paint);
+
+        text = "自由美利坚，枪战每一天";
+
+        //变色 方式1
+        paint.setTextSize(58);
+        offsetY += paint.getFontSpacing();
+
+        advance = paint.getRunAdvance(text, 0, text.length(), 0, text.length(), false, 6);
+        float advance2 = paint.getRunAdvance(text, 0, text.length(), 0, text.length(), false, 8);
+
+        paint.setColor(Color.BLACK);
+        canvas.drawText("自由美利坚，", offsetX, offsetY, paint);
+        paint.setColor(Color.RED);
+        canvas.drawText("枪战", offsetX + advance, offsetY, paint);
+        paint.setColor(Color.BLACK);
+        canvas.drawText("每一天", offsetX + advance2, offsetY, paint);
+
+        //变色 方式2
+        paint.setTextSize(58);
+        offsetY += paint.getFontSpacing();
+        paint.getTextBounds(text, 0, 2, textBounds);
+        textBounds.left = (int) (offsetX +  advance);
+        textBounds.right = (int) (offsetX + advance2);
+        textBounds.top += offsetY;
+        textBounds.bottom += offsetY;
+
+        canvas.drawText(text, offsetX, offsetY, paint);
+
+        canvas.save();
+        paint.setColor(Color.RED);
+        canvas.clipRect(textBounds);
+        canvas.drawText(text, offsetX, offsetY, paint);
+        canvas.restore();
+
+        text = "Aa Jj Ââ";
+        paint.setTextSize(160);
+        offsetY += paint.getFontSpacing() * 2;
+
+        Paint.FontMetrics fontMetrics = new Paint.FontMetrics();
+
+        paint.getFontMetrics(fontMetrics);
+
+        textBounds.left = 0;
+        textBounds.right = getWidth();
+        textBounds.top = (int) (offsetY + fontMetrics.ascent);
+        textBounds.bottom = /*(int) (offsetY + fontMetrics.bottom)*/ (int) offsetY;
+
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(ViewUtil.dip2px(getContext(), 3));
+        canvas.drawRect(textBounds, paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawText(text, offsetX, offsetY, paint);
+
+        paint.setTextSize(160);
+        offsetY += paint.getFontSpacing() * 2;
+
+       /* paint.getTextBounds(text, 0, text.length(), textBounds);
+        textBounds.left += offsetX;
+        textBounds.right += offsetX;
+        textBounds.top += offsetY;
+        textBounds.bottom += offsetY;*/
+
+
+        paint.getFontMetrics(fontMetrics);
+
+        textBounds.left = 0;
+        textBounds.right = getWidth();
+        textBounds.top = (int) (offsetY + fontMetrics.ascent);
+        textBounds.bottom = (int) (offsetY + fontMetrics.bottom);
+
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(ViewUtil.dip2px(getContext(), 3));
+        canvas.drawRect(textBounds, paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawText(text, offsetX, offsetY, paint);
+
+        paint.setTextSize(160);
+        offsetY += paint.getFontSpacing() * 2;
+
+        paint.getTextBounds(text, 0, text.length(), textBounds);
+        textBounds.left += offsetX;
+        textBounds.right += offsetX;
+        textBounds.top += offsetY;
+        textBounds.bottom += offsetY;
+
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(ViewUtil.dip2px(getContext(), 3));
+        canvas.drawRect(textBounds, paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
         canvas.drawText(text, offsetX, offsetY, paint);
 
     }
